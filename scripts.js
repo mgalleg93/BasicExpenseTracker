da32c99bc0c5861e960ec94e807c15ceadf58709
Miguel Gallegos <miguelgallegosgt@gmail.com>
2022-09-22 22:05:28 -0700

    Added new expense functions

--- scripts.js
+++ scripts.js
@@ -1,17 +1,37 @@
 let errors = [];
-let currentTotal = 83.05;
+let currentTotal = 0;
+const regex = /[+-]?\d+(\.\d+)?/g;
 
+let expenses = [
+    {
+        date: '2022-09-20',
+        expense: 'Frys',
+        cost: '83.05',
+        category: 'Groceries',
+        person: 'Miguel',
+        method: 'Credit Card'
+    },
+    {
+        date: '2022-09-21',
+        expense: 'Starbucks',
+        cost: '5.05',
+        category: 'Food-Out',
+        person: 'Miguel',
+        method: 'Credit Card'
+    },
+]
+
+//Function to submit the expense
 function submitExpense() {
     if(validateForm()) {
-        addExpense();
+        createExpense();
     } else {
         alert("Fix yo shit homie");
     }
 }
 
-function addExpense() {
-    // Create new table row element and all the variables that will go in it
-    let newRow = document.createElement('tr');
+//Function to create the new expense
+function createExpense() {
     let newDate = document.getElementById('expenseDate').value;
     let newExpense = document.getElementById('expenseName').value;
     let newCost = document.getElementById('expenseCost').value;
@@ -19,15 +39,45 @@ function addExpense() {
     let newPerson = document.getElementById('expenseUser').value;
     let newMethod = document.getElementById('expenseMethod').value;
 
-    newRow.innerHTML = `<th scope="row">${newDate}</th><td>${newExpense}</td><td>${newCost}</td><td>${newCategory}</td>
-                        <td>${newPerson}</td><td>${newMethod}</td>`;
+    let theExpense = {
+        date: newDate,
+        expense: newExpense,
+        cost: newCost,
+        category: newCategory,
+        person: newPerson,
+        method: newMethod
+    }
+
+    addExpense(theExpense);
+    clearForm();
+}
+
+//Function to clear the form once the expense has been added
+function clearForm() {
+    document.getElementById('expenseDate').value = '';
+    document.getElementById('expenseName').value = '';
+    document.getElementById('expenseCost').value = '';
+    document.getElementById('expenseCategory').value = '';
+    document.getElementById('expenseUser').value = '';
+    document.getElementById('expenseMethod').value ='';
+}
+
+//Function to add the new expense to the table
+function addExpense(item) {
+    // Create new table row element and all the variables that will go in it
+    let newRow = document.createElement('tr');
+    let theDate = new Date(item.date);
+
+    newRow.innerHTML = `<th scope="row">${theDate.toLocaleDateString()}</th><td>${item.expense}</td><td>$${item.cost}</td><td>${item.category}</td>
+                        <td>${item.person}</td><td>${item.method}</td>`;
     
     document.getElementById('tableBody').append(newRow);
-    currentTotal = currentTotal + parseInt(newCost);
+    currentTotal = currentTotal + parseFloat(item.cost);
     console.log(currentTotal);
     document.getElementById('total').innerText = '$' + currentTotal;
 }
 
+//Function to validate the form for inputing an expense
 function validateForm() {
     errors = [];
     const inputs = document.querySelectorAll('input');
@@ -48,4 +98,11 @@ function validateForm() {
     } else {
         return true;
     }
+}
+
+//Page on load functions and event listeners
+function loadingFunction() {
+    expenses.forEach(element => {
+        addExpense(element);
+    });
 }
